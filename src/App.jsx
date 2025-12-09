eb84e95e9808fcd2debb5ac08d34723afd6c7cd5
Tanja Heinz <heinz.tanjamaria@gmail.com>
2025-12-09 18:35:54 +0100

    function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello World!</h1>;
+function MyComponent(props) {
+  return <h1>Hello World out there, {props.name}!</h1>;
 }
 
-export default App;
+export default MyComponent;
